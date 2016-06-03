bf4d446bafb25b77cd0cc8a58092545bc8aad8e7
baeseohyeon <formnarago@naver.com>
2016-06-03 16:13:30 +0900

    배서현
    배배

--- minus.c
+++ minus.c
@@ -1,59 +1,3 @@
-<<<<<<< HEAD
-#include <stdio.h>
-#include <string.h>
-
-#define StoD(X) (X==0? 0:X-'0')
-
-void reverse(char *arr, int len);
-
-int main(){
-	char a[3][51]={0};
-	int len, i, j, down = 0;
-	printf("첫번째 수를 입력하시오 : ");
-	scanf("%s", a[0]);
-	printf("두번째 수를 입력하시오 : ");
-	scanf("%s", a[1]);
-
-	if(strlen(a[0])>strlen(a[1])){                                                               
-		len=strlen(a[0]);
-	}
-	else len=strlen(a[1]);
-
-	reverse(a[0], strlen(a[0]));
-	reverse(a[1], strlen(a[1]));
-
-	for(i=0; i<=len; i++){
-		if((StoD(a[0][i])-StoD(a[1][i]))<0)
-		{	
-		a[2][i]=(StoD(a[0][i])-StoD(a[1][i])+down+10) + '0';
-		down = -1; 
-		}
-		else 
-		{
-		a[2][i]=StoD(a[0][i])-StoD(a[1][i]) + down + '0';
-		down = 0;
-		}
-	}
-
-	if(a[2][len]=='0') a[2][len]=0; 
-
-	reverse(a[2], strlen(a[2]));
-
-	printf("차는 %s입니다\n", a[2]);
-
-	return 0;
-}
-
-void reverse(char *arr, int len){
-	char temp;
-	int i;
-	for(i=0; i<len/2; i++){
-		temp=arr[i];
-		arr[i]=arr[len-1-i];
-		arr[len-1-i]=temp;
-	}
-}
-=======
 #include<stdio.h>
 #include<string.h>
 #define MAX 100
@@ -100,4 +44,3 @@ int main(){
 	for(int i = start ; i < MAX; i++)	//출력
 		printf("%d",num[2][i]);
 }
->>>>>>> 50a4fe12bb3d302e21e6d8be117dd43bb58bf822
--- plus.c
+++ plus.c
@@ -1,52 +1,3 @@
-<<<<<<< HEAD
-#include <stdio.h>
-#include <string.h>
-
-#define StoD(X) (X==0? 0:X-'0')
-
-void reverse(char *arr, int len);
-
-int main(){
-	char a[3][51]={0};
-	int len, i, j, up=0;
-	printf("첫번째 수를 입력하시오 : ");
-	scanf("%s", a[0]);
-	printf("두번째 수를 입력하시오 : ");
-	scanf("%s", a[1]);
-
-	if(strlen(a[0])>strlen(a[1])){                                                               
-		len=strlen(a[0]);
-	}
-	else len=strlen(a[1]);
-
-	reverse(a[0], strlen(a[0]));
-	reverse(a[1], strlen(a[1]));
-
-	for(i=0; i<=len; i++){
-		a[2][i]=(StoD(a[0][i])+StoD(a[1][i])+up)%10+'0';
-		if((StoD(a[0][i])+StoD(a[1][i])+up)>9) up=1;
-		else up=0;
-	}
-
-	if(a[2][len]=='0') a[2][len]=0; 
-
-	reverse(a[2], strlen(a[2]));
-
-	printf("합은  %s입니다\n", a[2]);
-
-	return 0;
-}
-
-void reverse(char *arr, int len){
-	char temp;
-	int i;
-	for(i=0; i<len/2; i++){
-		temp=arr[i];
-		arr[i]=arr[len-1-i];
-		arr[len-1-i]=temp;
-	}
-}
-=======
 #include<stdio.h>
 #include<string.h>
 #define MAX 100
@@ -93,4 +44,3 @@ int main(){
 	for(int i = start ; i < MAX; i++)	//출력
 		printf("%d",num[2][i]);
 }
->>>>>>> 50a4fe12bb3d302e21e6d8be117dd43bb58bf822
