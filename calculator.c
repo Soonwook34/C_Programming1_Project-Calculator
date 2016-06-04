9714ed3a62b24de9973d0a5c213e3c179d0509fc
soonwook0304 <soonwook34@gmail.com>
2016-06-05 08:52:41 +0900

    Soonwook_005
    함수 구분, 오류 처리, 덧셈 유효숫자 출력 추가

--- calculator.c
+++ calculator.c
@@ -13,66 +13,152 @@
 
 #define N 100
 
-void inputNum(char input[]);
-void reverse(char rev[], int n);
-void integer(char inte[], int n);
-int plus(char a[], int n, char b[], int m, char answer[]);
+void starter(char input[]);										//수식 입력 함수
+void calculate(char value[], char answer[]);					//수식 계산 함수
+int plus(char a[], int n, char b[], int m, char answer[]);		//덧셈 함수
+int minus(char a[], int n, char b[], int m, char answer[]);		//뺄셈 함수
+int multiply(char a[], int n, char b[], int m, char answer[]);	//곱셈 함수
+int divide(char a[], int n, char b[], int m, char answer[]);	//나눗셈 함수
+int mod(char a[], int n, char b[], int m, char answer[]);		//나머지 함수
+void integer(char inte[], int n);								//받은 배열(상수)을 char형에서 int형으로 바꾸는 함수
+void reverse(char rev[], int n);								//받은 배열(상수)을 뒤집어 주는 함수 
+void printAnswer(char answer[], int length, int sigDigit);		//정답 출력 함수
 
 int main()
 {
 	while(1){
+		//처음 수식을 받을 배열
 		char input[10000] = {0};
-		inputNum(input);
+
+		//수식 입력 함수 호출
+		starter(input);
+
+		//end 처리
 		if(input[0]=='e' && input[1]=='n' && input[2]=='d' && input[3]=='\0'){
 			break;
 		}
+
+		//clear 처리
 		if(input[0]=='c' && input[1]=='l' && input[2]=='e' && input[3]=='a' && input[4]=='r' && input[5]=='\0'){
 			system("clear");
+		}
+
+		//save 처리
+		if(input[0]=='s' && input[1]=='a' && input[2]=='v' && input[3]=='e' && input[4]=='\0'){
+			printf("save 함수 만들어야지..\n\n");
+		}
+
+		//load 처리
+		if(input[0]=='l' && input[1]=='o' && input[2]=='a' && input[3]=='d' && input[4]=='\0'){
+			printf("load 함수 만들어야지..\n\n");
+		}
+
+		//VAR 처리
+		if(input[0]=='V' && input[1]=='A' && input[2]=='R' && input[3]=='\0'){
+			printf("VAR 함수 만들어야지..\n\n");
 		}
 	}
 	return 0;
 }
 
-void inputNum(char input[])
+//수식 계산 함수
+void starter(char input[])
 {
-	//수와 문자와 기호를 저장할 배열
-	char value[N][59] = {0};
-	//소수
-	char deciCount[N][2] = {0};
-	int c, i=0, j, k, n, m, l;
+	int c, i=0;
+	//답 저장 배열
+	char answer[59] = {0};
 
 	printf("입력 : ");
 
 	//처음 입력 받기
 	while(1){
-		//수식 입력의 마지막 처리
 		c = getchar();
-		if((c=='\n') || (c==EOF)){
+
+		//수식 입력의 마지막 처리
+		if(c=='\n'){
 			input[i] = '\0';
 			break;
 		}
+
 		//띄어쓰기 무시
-		else if(c==' '){
+		else if((c==' ') || (c=='	')){
 			continue;
 		}
-		//받을 수 있는 것(숫자, 알파벳, 수식 기호, 소수점)은 input배열에 저장한다
-		else if(((c>='0') && (c<='9')) || (c>='a') && (c<='z') || (c>='A') && (c<='Z') || (c=='+') || (c=='-') || (c=='*') || (c=='/') || (c=='%') || (c=='=') || (c=='.')){
+
+		//받을 수 있는 것(숫자, 알파벳, 수식 기호, 소수점)은 input배열에 저장
+		else if(((c>='0') && (c<='9')) || ((c>='a') && (c<='z')) || ((c>='A') && (c<='Z')) || (c=='+') || (c=='-') || (c=='*') || (c=='/') || (c=='%') || (c=='=') || (c=='.')){
 			input[i] = c;
 			i++;
 		}
-		//나머지를 받았을 경우
+
+		//기타 문자 오류 처리
 		else{
-			printf("error");
+			printf("error : '%c'는 인식할 수 없습니다\n\n",c);
+			while(1){
+				c = getchar();
+				if(c=='\n'){
+					break;
+				}
+			}
 			return;
 		}
 	}
 
+	//end이면 함수 종료
+	if(input[0]=='e' && input[1]=='n' && input[2]=='d' && input[3]=='\0'){
+		return;
+	}
+
+	//clear이면 함수 종료
+	else if(input[0]=='c' && input[1]=='l' && input[2]=='e' && input[3]=='a' && input[4]=='r' && input[5]=='\0'){
+		return;
+	}
+
+	//save이면 함수 종료
+	else if(input[0]=='s' && input[1]=='a' && input[2]=='v' && input[3]=='e' && input[4]=='\0'){
+		return;
+	}
+
+	//load이면 함수 종료
+	else if(input[0]=='l' && input[1]=='o' && input[2]=='a' && input[3]=='d' && input[4]=='\0'){
+		return;
+	}
+
+	//VAR이면 함수 종료
+	else if(input[0]=='V' && input[1]=='A' && input[2]=='R' && input[3]=='\0'){
+		return;
+	}
+
+	//수식 계산 함수 호출
+	calculate(input, answer);
+
+	return;
+}
+
+//수식 계산 함수
+void calculate(char input[], char answer[])
+{
+	int c, i, j, k, l;
+	int v1, v2;
+	int length, sigDigit=0;
+	//수와 문자와 기호를 저장할 배열
+	char value[N][59] = {0};
+	//소수
+	char deciCount[N][2] = {0};
+
 	//수식 구분하기
-	for(i=0, j=0, k=0, l=0; i<strlen(input); i++){
+	for(i=0, j=0, k=0; i<strlen(input); i++){
 		c = input[i];
 
 		//숫자
 		if((c>='0') && (c<='9')){
+			if(k>=1){
+				if(((value[j][k-1]>='a') && (value[j][k-1]<='z')) || ((value[j][k-1]>='A') && (value[j][k-1]<='Z'))){
+					printf("error : 수식을 잘못 입력하셨습니다\n\n");
+					return;
+				}
+			}
+			//소수 카운트
 			if(deciCount[j][0]){
 				deciCount[j][1]+=1;
 			}
@@ -81,10 +167,12 @@ void inputNum(char input[])
 		}
 
 		//알파벳
-		else if((c>='a') && (c<='z') || (c>='A') && (c<='Z')){
-			if(deciCount[j][0]){
-				printf("error\n");
-				break;
+		else if(((c>='a') && (c<='z')) || ((c>='A') && (c<='Z'))){
+			if(k>=1){
+				if((((value[j][k-1]>='0') && (value[j][k-1]<='9')) || deciCount[j][0]) || (((value[j][k-1]>='a') && (value[j][k-1]<='z')) || ((value[j][k-1]>='A') || (value[j][k-1]<='Z')))){
+					printf("error : 수식을 잘못 입력하셨습니다\n\n");
+					return;
+				}
 			}
 			value[j][k] = c;
 			k++;
@@ -92,8 +180,9 @@ void inputNum(char input[])
 
 		//수식기호
 		else if((c=='+') || (c=='-') || (c=='*') || (c=='/') || (c=='%') || (c=='=')){
+			//전에 배열이 상수라면 소수자리 채우기
 			if(value[j][0]>='0' && value[j][0]<='9'){
-				for(m=deciCount[j][1];m<9;m++){
+				for(l=deciCount[j][1];l<9;l++){
 					value[j][k]='0';
 					k++;
 				}
@@ -107,8 +196,8 @@ void inputNum(char input[])
 		//소수점
 		else if (c=='.'){
 			if(deciCount[j][0]){
-				printf("error\n");
-				break;
+				printf("error : 소수를 잘못 입력하셨습니다\n\n");
+				return;
 			}
 			deciCount[j][0]=1;
 		}
@@ -118,98 +207,90 @@ void inputNum(char input[])
 			value[j][k]='\0';
 			break;
 		}
-		//자리수가 초과하는지 검사
+
+		//소수 최대 자리를 초과했는지 검사
 		if(deciCount[j][0]){
 			if(deciCount[j][1]==10){
-				printf("decimal place error\n\n\n");
-				break;
+				printf("error : 소수부분은 9자리까지만 입력해주세요\n\n");
+				return;
 			}
 		}
+
+		//정수 최대 자리를 초과했는지 검사
 		else{
 			if(k==51){
-				printf("integer place error\n\n\n");
-				break;
+				printf("error : 정수부분은 50자리까지만 입력해주세요\n\n");
+				return;
 			}
 		}
 	}
 
-	//마지막 수의 소수자리 처리
+	//마지막 배열의 소수자리 처리
 	if(value[j][0]>='0' && value[j][0]<='9'){
-		for(m=deciCount[j][1];m<9;m++){
+		for(l=deciCount[j][1];l<9;l++){
 			value[j][k]='0';
 			k++;
 		}
 	}
 
-	//end
-	if(input[0]=='e' && input[1]=='n' && input[2]=='d' && input[3]=='\0'){
-		return;
-	}
-
-	//clear
-	if(input[0]=='c' && input[1]=='l' && input[2]=='e' && input[3]=='a' && input[4]=='r' && input[5]=='\0'){
-		return;
-	}
-	//나눈 배열 확인
-	/*for(k=0; k<j+1; k++){
-		printf("Value%d = %s, %d\n", k, value[k], deciCount[k][1]);
-	}
-
-	//배열의 길이 확인
-	for(k=0; k<j+1; k++){
-		printf("%d\n", strlen(value[k]));
-	}*/
-
-	char answer[59] = {0};
-	int v1=strlen(value[0]);
-	int v2=strlen(value[2]);
-	int tmp;
+	v1=strlen(value[0]);
+	v2=strlen(value[2]);
+	
+	//'+'일 경우
 	if(value[1][0]=='+'){
-		integer(value[0],v1);
-		integer(value[2],v2);
-		reverse(value[0],v1);
-		reverse(value[2],v2);
+		length = plus(value[0],v1,value[2],v2,answer);
 
-		tmp = plus(value[0],v1,value[2],v2,answer);
+		reverse(answer,length);
 
-		reverse(answer,tmp);
-		
-		printf("답 : ");
-		for(l=0, i=0; i<tmp; i++){
-			if(i==tmp-9){
-				printf(".");
-			}
-			else if(l%3==tmp%3 && l!=0 && i!=1){
-				printf(",");
-			}
-			printf("%d", answer[i]);
-			l++;
+		if(deciCount[0][1]>=deciCount[2][1]){
+			sigDigit = deciCount[0][1];
+		}
+		else{
+			sigDigit = deciCount[2][1];
 		}
 	}
-	//출력
-	/*printf("전체 수식 : ");
-	for(k=0, l=0; k<j+1; k++){
-		for(n=0; n<strlen(value[k]); n++){
-			if(n==strlen(value[k])-9){
-				printf(".");
-			}
-			else if(l%3==strlen(value[k])%3 && l!=0 && strlen(value[k])!=1){
-				printf(",");
-			}
-			printf("%d", value[k][n]);
-			l++;
-		}
-		printf(" ");
-		l=0;
-	}*/
-	printf("\n");
+
+	//'-'일 경우
+	if(value[1][0]=='-'){
+		length = minus(value[0],v1,value[2],v2,answer);
+	}
+
+	//'*'일 경우
+	if(value[1][0]=='*'){
+		length = multiply(value[0],v1,value[2],v2,answer);
+	}
+
+	//'/'일 경우
+	if(value[1][0]=='/'){
+		length = divide(value[0],v1,value[2],v2,answer);
+	}
+
+	//'%'일 경우
+	if(value[1][0]=='%'){
+		length = mod(value[0],v1,value[2],v2,answer);
+	}
+
+	//정답 출력 함수 호출
+	printAnswer(answer,length,sigDigit);
+
+	printf("\n\n");
 	return;
 }
 
+//덧셈 함수
 int plus(char a[], int n, char b[], int m, char answer[])
 {
 	int i, j;
-	
+
+	//받은 배열(상수)를 char형에서 int형으로 바꾼다
+	integer(a,n);
+	integer(b,m);
+
+	//받은 배열(상수)을 뒤집는다
+	reverse(a,n);
+	reverse(b,m);
+
+	//소수자리를 위한 수
 	if(n>=m){
 		j=n;
 	}
@@ -217,9 +298,12 @@ int plus(char a[], int n, char b[], int m, char answer[])
 		j=m;
 	}
 
+	//두 수를 한자리씩 더해준다
 	for(i=0;i<j;i++){
 		answer[i] = a[i] + b[i];
 	}
+
+	//10이 넘어가면 올려준다
 	for(i=0;i<=j;i++){
 		if(answer[i] >= 10){
 			answer[i] -= 10;
@@ -229,9 +313,50 @@ int plus(char a[], int n, char b[], int m, char answer[])
 	return j;
 }
 
+//뺄셈 함수
+int minus(char a[], int n, char b[], int m, char answer[])
+{
+	printf("뺄셈 함수 입력해야지...\n");
+	return 0;
+}
+
+//곱셈 함수
+int multiply(char a[], int n, char b[], int m, char answer[])
+{
+	printf("곱셈 함수 입력해야지...\n");
+	return 0;
+}
+
+//나눗셈 함수
+int divide(char a[], int n, char b[], int m, char answer[])
+{
+	printf("나눗셈 함수 입력해야지...\n");
+	return 0;
+}
+
+//나머지 함수
+int mod(char a[], int n, char b[], int m, char answer[])
+{
+	printf("나머지 함수 입력해야지...\n");
+	return 0;
+}
+
+//받은 배열(상수)을 char형에서 int형으로 바꾸는 함수
+void integer(char inte[], int n)
+{
+	int i;
+	for(i=0;i<n;i++){
+		if(inte[i]!=0){
+			inte[i] -= '0';
+		}
+	}
+	return;
+}
+
+//받은 배열(상수)을 뒤집어 주는 함수
 void reverse(char rev[], int n)
 {
-	char tmp;
+	int tmp;
 	int i;
 	for(i=0;i<n/2;i++){
 		tmp = rev[i];
@@ -241,13 +366,36 @@ void reverse(char rev[], int n)
 	return;
 }
 
-void integer(char inte[], int n)
+//정답 출력 함수
+void printAnswer(char answer[], int length, int sigDigit)
 {
-	int i;
-	for(i=0;i<n;i++){
-		if(inte[i]!=0){
-			inte[i] -= '0';
+	int i, j;
+	printf("답 : ");
+	for(i=0, j=0; i<length; i++){
+		//소수자리 출력
+		if(i==length-9){
+			if(sigDigit!=0){
+				printf(".");
+				for(j=0; i<length+sigDigit-9; i++){
+					printf("%d", answer[i]);
+					j++;
+					if((j%3==0) && j!=sigDigit){
+						printf(",");
+					}
+				}
+				return;
+			}
+			else{
+				return;
+			}
 		}
+		//콤마 출력
+		else if((j%3==length%3) && (j!=0 && length!=1)){
+			printf(",");
+		}
+		//정답 출력
+		printf("%d", answer[i]);
+		j++;
 	}
 	return;
 }
