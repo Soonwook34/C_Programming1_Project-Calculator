5bf2c22a4c949b7449ccf1b5481f366b2ef41da7
soonwook0304 <soonwook34@gmail.com>
2016-06-13 23:41:41 +0900

    sync
    SYNC!

--- calculator.c
+++ calculator.c
@@ -12,38 +12,6 @@
 #include <string.h>
 
 #define N 10
-<<<<<<< HEAD
-
-void starter(char input[]);													//수식 입력 함수
-void calculate(char value[], char answer[]);								//수식 계산 함수
-int compare(char a[], int v1, char b[], int v2);							//두 배열 크기 비교
-<<<<<<< HEAD
-
-=======
->>>>>>> master
-int plus(char a[], int n, char b[], int m, char answer[]);					//덧셈 함수
-int minus(char a[], int n, char b[], int m, char answer[]);					//뺄셈 함수
-int multiply(char a[], int n, char b[], int m, char answer[]);				//곱셈 함수
-void divide(char a[], int n, char b[], int m, char answer[], int check[]);	//나눗셈 함수
-void mod(char a[], int n, char b[], int m, char answer[], int check[]);		//나머지 함수
-void copy(char a[],char b[]);												//복붙
-void multiplyTen(char a[]);													//10배 함수
-<<<<<<< HEAD
-
-void variable(char varName[], char value[], int length, int negCheck, char deciCount[], int saveVariable[], int checkVar[]);
-void VAR(int checkVar[], int saveVariable[]);								//VAR 함수
-void save(int checkVar[], int saveVariable[]);								//save 함수
-void load(int checkVar[], int saveVariable[]);								//load 함수
-
-void integer(char inte[], int n);											//받은 배열(상수)을 char형에서 int형으로 바꾸는 함수
-void reverse(char rev[], int n);											//받은 배열(상수)을 뒤집어 주는 함수 
-=======
-void variable(char varName[], char value[], int length, int negCheck, char deciCount[], int saveVariable[], int checkVar[]);
-void integer(char inte[], int n);											//받은 배열(상수)을 char형에서 int형으로 바꾸는 함수
-void reverse(char rev[], int n);											//받은 배열(상수)을 뒤집어 주는 함수 
-void printanswer(char answer[], int length, int sigDigit, int negNum);		//정답 출력 함수
->>>>>>> master
-=======
 
 void starter(char input[]);																									//수식 입력 함수
 void calculate(char value[], char answer[]);																				//수식 계산 함수
@@ -70,12 +38,7 @@ void printanswer(char answer[], int length, int sigDigit, int negNum);										
 //변수 관련 배열
 static int saveVariable[26][62] = {0};
 static int checkVar[26] = {0};
->>>>>>> refs/remotes/origin/hyeyun
 
-void printanswer(char answer[], int length, int sigDigit, int negNum);		//정답 출력 함수
-
-static int saveVariable[26][62] = {0};
-static int checkVar[26] = {0};
 //메인 함수
 int main()
 {
@@ -176,24 +139,10 @@ void calculate(char input[], char answer[])
 	int c, i=0, j, k, l, temp;
 	int v1, v2;
 	int length, sigDigit=0, negNum=0, negCheck=0;
-<<<<<<< HEAD
-	int check[2] = {0};
-=======
->>>>>>> refs/remotes/origin/hyeyun
 	//수와 문자와 기호를 저장할 배열
 	char value[N][60] = {0};
 	//소수
 	char deciCount[N][2] = {0};
-<<<<<<< HEAD
-	//변수 받기
-<<<<<<< HEAD
-=======
-	static int saveVariable[26][62] = {0};
-	static int checkVar[26] = {0};
->>>>>>> master
-
-=======
->>>>>>> refs/remotes/origin/hyeyun
 	//수식 구분하기
 	if(input[0]=='-'){
 		negCheck+=1;
@@ -288,50 +237,6 @@ void calculate(char input[], char answer[])
 
 	v1=strlen(value[0]);
 	v2=strlen(value[2]);
-	if(value[1][0]=='+' || value[1][0]=='-' || value[1][0]=='*' || value[1][0]=='/' || value[1][0]=='%'){
-		for(i=0;i<3;i+=2){
-			if(((value[i][0]>='A') && (value[i][0]<='Z')) || ((value[i][0]>='a') && (value[i][0]<='z'))){
-				if(value[i][0]>='a'){
-					temp=value[i][0]-'a';
-				}
-				else{
-					temp=value[i][0]-'A';
-				}
-				//변수가 저장되있는지 확인
-				if(checkVar[temp]){
-					//전체 자리수 저장
-					length = saveVariable[temp][59]-saveVariable[temp][61]+9;
-					//변수 출력
-					for(j=0;j<length;j++){
-						if(saveVariable[temp][j]!=0){
-							value[i][j] = saveVariable[temp][j]+'0';
-						}
-						else{
-							value[i][j] = '0';
-						}
-					}
-					//부호 저장
-					negCheck += saveVariable[temp][60];
-					//소수자리 저장
-					if(saveVariable[temp][61]){
-						deciCount[i][0] = 1;
-						deciCount[i][1] = saveVariable[temp][61];
-					}
-					if(i){
-						v2=length;
-					}
-					else{
-						v1=length;
-					}
-				}
-				else{
-					printf("error : %c는 선언되지 않았습니다 변수 집어넣기 오류\n\n",value[i][0]);
-					return;
-				}
-
-			}
-		}
-	}
 
 	if(value[1][0]=='+' || value[1][0]=='-' || value[1][0]=='*' || value[1][0]=='/' || value[1][0]=='%'){
 		for(i=0;i<3;i+=2){
@@ -475,38 +380,6 @@ void calculate(char input[], char answer[])
 	}
 	//'/'일 경우
 	else if(value[1][0]=='/'){
-<<<<<<< HEAD
-		divide(value[0],v1,value[2],v2,answer,check);
-		length = check[0];
-		sigDigit = check[1];
-	}
-	//'%'일 경우
-	else if(value[1][0]=='%'){
-		mod(value[0],v1,value[2],v2,answer,check);
-		length = check[0];
-		sigDigit = check[1];
-	}
-	//'='일 경우
-	else if(value[1][0]=='='){
-		if(((value[0][0]>='A') && (value[0][0]<='Z')) || ((value[0][0]>='a') && (value[0][0]<='z'))){
-			v2=strlen(value[2]);
-
-			variable(value[0], value[2], v2, negCheck, deciCount[2], saveVariable, checkVar);
-
-			length = v2;
-
-			if(negCheck>1){
-				negNum = 1;
-			}
-
-			sigDigit = deciCount[2][1];
-
-			for(i=0;i<v2;i++){
-				answer[i] = value[2][i];
-			}
-		}
-	}
-=======
 		length = divide(value[0],v1,value[2],v2,answer);
 		int a = length;
 		while(answer[--a] == 0);
@@ -542,7 +415,6 @@ void calculate(char input[], char answer[])
 			}
 		}
 	}
->>>>>>> refs/remotes/origin/hyeyun
 	//변수를 불러오는 경우
 	else if(((value[0][0]>='A') && (value[0][0]<='Z')) || ((value[0][0]>='a') && (value[0][0]<='z'))){
 		if(value[0][0]>='a'){
@@ -565,11 +437,7 @@ void calculate(char input[], char answer[])
 			sigDigit = saveVariable[temp][61];
 		}
 		else{
-<<<<<<< HEAD
-			printf("error : %c는 선언되지 않았습니다 변수 불러오기 오류\n\n",value[0][0]);
-=======
 			printf("error : %c는 선언되지 않았습니다\n\n",value[0][0]);
->>>>>>> refs/remotes/origin/hyeyun
 			return;
 		}
 	}	
@@ -583,28 +451,7 @@ void calculate(char input[], char answer[])
 	printf("\n\n");
 	return;
 }
-//두 배열 비교 함수
-int compare(char a[], int v1, char b[], int v2){
-	int i;
-	//앞이 크면 1을 리턴한다
-	if(v1 > v2)
-		return 1;
-	//뒤가 크면 2를 리턴한다
-	else if(v1 < v2)
-		return 2;
-	else
-		for(i=0 ; i < v1; i++){
-			if(a[i] > b[i])
-				return 1;
-			else if(a[i] < b[i])
-				return 2;
-		}
-	//같으면 0을 리턴한다
-	return 0;
 
-<<<<<<< HEAD
-}
-=======
 //두 배열 비교 함수
 int compare(char a[], int v1, char b[], int v2){
 	int i;
@@ -625,7 +472,6 @@ int compare(char a[], int v1, char b[], int v2){
 	return 0;
 }
 
->>>>>>> refs/remotes/origin/hyeyun
 //덧셈 함수
 int plus(char a[], int n, char b[], int m, char answer[])
 {
@@ -767,15 +613,6 @@ int multiply(char a[], int n, char b[], int m, char answer[])
 	}
 
 	//전체 자리를 위한 수를 리턴한다
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-	for(i=0;i<60;i++){
-		printf("%d",temp[0][i]);
-	}
->>>>>>> master
-=======
->>>>>>> refs/remotes/origin/hyeyun
 	j=m+n;
 	for(i=0;i<j;i++){
 		temp[0][i]=temp[0][i+9];
@@ -801,7 +638,7 @@ int multiply(char a[], int n, char b[], int m, char answer[])
 }
 
 //나눗셈 함수
-void divide(char a[], int n, char b[], int m, char answer[], int check[])
+int divide(char a[], int n, char b[], int m, char answer[])
 {
 	char ta[60];
 	char tb[60];
@@ -824,14 +661,7 @@ void divide(char a[], int n, char b[], int m, char answer[], int check[])
 	while(howBig != -10){
 		int count = 0;
 		char getAnswer[60] = {0};
-<<<<<<< HEAD
-<<<<<<< HEAD
-		
-=======
->>>>>>> master
-=======
 
->>>>>>> refs/remotes/origin/hyeyun
 		while(compare(ta,strlen(ta),tb,strlen(tb)) == 1){
 			int len = minus(ta,strlen(ta),tb,strlen(tb),getAnswer);
 			for(int i = 0; i < len / 2;i++){
@@ -844,13 +674,6 @@ void divide(char a[], int n, char b[], int m, char answer[], int check[])
 			copy(ta,getAnswer);
 			copy(tb,tbb);
 			count++;	
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
->>>>>>> master
-=======
->>>>>>> refs/remotes/origin/hyeyun
 		}	
 		if(compare(ta,strlen(ta),tb,strlen(tb)) == 0){
 			count ++;
@@ -867,69 +690,18 @@ void divide(char a[], int n, char b[], int m, char answer[], int check[])
 		answer[i] = answer[j - i - 1];
 		answer[j - i - 1] = t;
 	}
-<<<<<<< HEAD
-	check[0] = j;
-	check[1] = 9;
-<<<<<<< HEAD
-	return;
-}
-
-//나머지 함수
-void mod(char a[], int n, char b[], int m, char answer[], int check[]){
-=======
-}
-
-//나머지 함수
-void mod(char a[], int n, char b[], int m, char answer[], int check[])
-{
->>>>>>> master
-=======
 	//전체 자리수 리턴
 	return j;
 }
 
 //나머지 함수
 int mod(char a[], int n, char b[], int m, char answer[]){
->>>>>>> refs/remotes/origin/hyeyun
 	char ta[60];
 	char tb[60];
 	char temp[60];
 	copy(ta,a);
 	copy(tb,b);
 	char getAnswer[60] = {0};
-<<<<<<< HEAD
-	int len;
-	divide(ta,strlen(ta),tb,strlen(tb),getAnswer,check);
-	len = check[0];
-	for(int i = 0; i < 9; i++)
-		getAnswer[len - i - 1] = 0;
-	for(int i =0; i < len; i++)
-		getAnswer[i] += '0';
-	copy(temp,getAnswer);
-	len = multiply(temp,strlen(temp),tb,strlen(tb),getAnswer);
-	for(int i = 0; i < len/2; i++){
-		int t = getAnswer[i];
-		getAnswer[i] = getAnswer[len - i -1];
-		getAnswer[len - i - 1] = t;
-	}
-	for(int i = 0; i < len; i++)
-		getAnswer[i] += '0';
-	copy(tb,getAnswer);
-<<<<<<< HEAD
-=======
-
->>>>>>> master
-	len = minus(ta,strlen(ta),tb,strlen(tb),answer);
-	for(int i = 0; i < len / 2; i++){
-		int t = answer[i];
-		answer[i] = answer[len - i - 1];
-		answer[len - i - 1] = t;
-	}
-	check[0] = len;
-	check[1] = 9;
-	return;
-}
-=======
 	int len,i,t;
 	len = divide(ta,strlen(ta),tb,strlen(tb),getAnswer);
 	for(i = 0; i < 9; i++)
@@ -956,17 +728,11 @@ int mod(char a[], int n, char b[], int m, char answer[]){
 	return len;
 }
 
->>>>>>> refs/remotes/origin/hyeyun
 //배열 복사 함수
 void copy(char a[], char b[]){
 	for(int i = 0; i < 60; i++)
 		a[i]  = b[i];
 }
-<<<<<<< HEAD
-//10배 함수
-void multiplyTen(char a[]){
-	a[strlen(a)] = '0';
-=======
 
 //10배 함수
 void multiplyTen(char a[]){
@@ -1103,136 +869,8 @@ void load(int checkVar[], int saveVariable[]){
 	}
 	fclose(load);
 	return;
->>>>>>> refs/remotes/origin/hyeyun
 }
-<<<<<<< HEAD
-//변수 정의 함수
-=======
->>>>>>> master
-void variable(char varName[], char value[], int length, int negCheck, char deciCount[], int saveVariable[], int checkVar[]){
-	static int varCount = 0;
-	int i,j,temp;
 
-	integer(value, length);
-
-	if(varName[0]>='a'){
-		temp=varName[0]-'a';
-	}
-	else{
-		temp=varName[0]-'A';
-	}
-	//변수 개수 카운트
-	if(checkVar[temp]==0){
-		varCount++;	
-		checkVar[temp] = 1;
-	}
-	//변수를 10개 이상 선언
-	if(varCount>10){
-		printf("error : 변수는 10개까지 입력 받을 수 있습니다.\n\n");
-		return;
-	}
-	//변수 저장
-	for(i=0;i<length;i++){
-		saveVariable[temp*62+i] = value[i];
-	}
-	//소수자리 저장
-	saveVariable[(temp+1)*62-1] = deciCount[1];
-	//부호 저장
-	if((negCheck==1) || (negCheck==2)){
-		saveVariable[(temp+1)*62-2] = 1;
-	}
-	//전체 자리수 저장
-	saveVariable[(temp+1)*62-3] = length-9+deciCount[1];
-	return;
-<<<<<<< HEAD
-}
-//VAR 함수
-void VAR(int checkVar[], int saveVariable[]){
-	int i,j;
-	for(i=0;i<26;i++){
-		//변수가 정의되어있는지 확인
-		if(checkVar[i]){
-			printf("%c = ", i+'A');
-			//변수 저장
-			if(saveVariable[(i+1)*62-2]){		
-				printf("-");		
-			}
-			for(j=0;j<saveVariable[(i+1)*62-3];j++)
-			{
-				//소수점 찍기
-				if(j==saveVariable[(i+1)*62-3]-saveVariable[(i+1)*62-1]){
-					printf(".");
-				}
-				printf("%d",saveVariable[i*62+j]);
-			}
-			printf("\n");
-		}
-	}
-	printf("\n");
-	return;
-=======
->>>>>>> master
-}
-//save 함수
-void save(int checkVar[], int saveVariable[]){
-	FILE *save;
-	int i,j;
-
-	save = fopen("VAR","w");
-	for(i=0;i<26;i++){
-		//변수가 선언되어있는지 확인
-		if(checkVar[i]){
-			fprintf(save,"%c = ", i+'A');
-			//변수 저장
-			if(saveVariable[(i+1)*62-2]){		
-				fprintf(save,"-");		
-			}
-
-			for(j=0;j<saveVariable[(i+1)*62-3];j++)
-			{
-				//소수점 찍기
-				if(j==saveVariable[(i+1)*62-3]-saveVariable[(i+1)*62-1]){
-					fprintf(save,".");
-				}
-				fprintf(save,"%d",saveVariable[i*62+j]);
-			}
-			fprintf(save,"\n");
-		}
-	}
-	fclose(save);
-	return ;
-}
-//load 함수
-void load(int checkVar[], int saveVariable[]){
-	FILE *load;
-	int i,j,k,deci=0;
-	int name;
-	char temp[61] = {0};
-	load = fopen("VAR","r");
-	while((fscanf(load, "%c = %s\n",&name, &temp)!=EOF))
-	{
-		checkVar[name-'A'] = 1;
-		for(i=0,j=0,k=0,deci=0;i<strlen(temp);i++){
-			if(temp[i]>='0' && temp[i]<='9'){
-				saveVariable[(name-'A')*62+j]=temp[i]-'0';
-				j++;
-				if(deci){
-					k++;
-				}
-			}
-			else if(temp[i]=='.'){
-				deci=1;
-			}
-			else if(temp[i]=='-'){
-				saveVariable[(name-'A'+1)*62-2]=1;
-			}
-		}
-		saveVariable[(name-'A'+1)*62-3]=j;
-		saveVariable[(name-'A'+1)*62-1]=k;
-	}
-	fclose(load);
-	return;
-}
 //받은 배열(상수)을 char형에서 int형으로 바꾸는 함수
 void integer(char inte[], int n)
 {
